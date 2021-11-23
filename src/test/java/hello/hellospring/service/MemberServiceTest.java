2d63a8f52d36067c69b2b134f8591e489bae2b04
hyejin <keephopealive@naver.com>
2021-11-23 23:39:01 +0900

    AWS RDS(maria db) 연결 * JPA

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -6,13 +6,21 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
 
+    @Autowired
     MemberService memberService;
+    @Autowired
     MemoryMemberRepository memberRepository;
 
     @BeforeEach // 각 테스트 실행 전 객체 생성. MemberService와 같은 인스턴스로 테스트 진행 (맞나??)
